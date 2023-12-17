9ee6e07f105e2163506bc8ff8d81c461927c5daa
MarsaColombo <timbiaou@gmail.com>
2023-12-17 19:17:27 +0100

    Added the Program file with changes

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -1,11 +1,39 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace DiceGame{
+namespace DiceGame
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+
+    }
+    public class Dice
+    {
+        private static Random random = new Random();
+
+        public int Roll()
+        {
+            int randomNumber = random.Next(0, 7);
+
+            if (randomNumber == 0)
+            {
+                return 1;
+            }
+            else if (randomNumber == 7)
+            {
+                return 6;
+            }
+            else
+            {
+                return randomNumber;
+            }
         }
     }
 }
+
+
